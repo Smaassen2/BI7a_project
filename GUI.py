5faba0539f602ac6e13751d3b9b772f66068a365
toscafrederiks <TC.Frederiks@student.han.nl>
2022-03-17 14:18:07 +0100

    eerste opzetje

--- GUI.py
+++ GUI.py
@@ -1,47 +1,29 @@
-import tkinter
+import tkinter as tk
 from tkinter import messagebox
 
+#window aanmaken
+window = tk.Tk()
+#titel window
+window.title("ORF finder & BLASTN")
+#afmetingen window
+window.geometry("500x300")
 
-class GUI:
-    def _init_(self):
-        # Maak de window aan
-        self.main_window = tkinter.Tk()
+#labels maken
+dna_seq_invoer = tk.Label(text="Voer hier je DNA sequentie in: ")
+#label packen
+dna_seq_invoer.pack()
 
-        # Maak twee frames aan (top en bottom)
-        self.top_frame = tkinter.Frame(self.main_window)
-        self.bottom_frame = tkinter.Frame(self.main_window)
+#invoerveld maken
+entry = tk.Entry()
+#invoerveld packen
+entry.pack()
 
-        # Maak een label aan
-        self.label1 = tkinter.Label(self.top_frame,
-                                    text="Was het maar vakantie!")
-        self.label2 = tkinter.Label(self.top_frame,
-                                    text="Hello world")
-        # Maak een button aan
-        self.knopje = tkinter.Button(self.bottom_frame,  # Waar komt de knop
-                                     text="Klik hier!",  # Text in de knop
-                                     command=self.do_something) # Wat te doen bij klik
-        self.quit_button = tkinter.Button(self.bottom_frame,
-                                          text= "quit",
-                                          command=self.main_window.destroy)
-        # PLaats de label in de GUI
-        self.label1.pack()
-        self.label2.pack()
-
-        # Plaats de frames in de GUI
-        self.top_frame.pack()
-        self.bottom_frame.pack()
-
-        # Plaats de buttons in de GUI
-        self.knopje.pack(side = "left")
-        self.quit_button.pack(side="left")
-
-        # Zorg dat de GUI tevoorschijn komt
-        tkinter.mainloop()
-
-    def do_something(self):
-        tkinter.messagebox.showinfo("Response",
-                                    "Bedankt voor het klikken")
+#button maken
+button = tk.Button(text="Start opdracht")
+#button packen
+button.pack()
 
 
-if __name__ == '__main__':
-    myGUI = GUI()
+
+#window weergeven
+window.mainloop()
