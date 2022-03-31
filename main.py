72a4725624777961df1cbb32284641997ed05167
toscafrederiks <70904519+toscafrederiks@users.noreply.github.com>
2022-03-31 13:50:37 +0200

    Update main.py
    Versie 8

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from BLASTN import BLAST
 
 
 if __name__ == '__main__':
+    # insert the DNA sequence to use
     seq = "ATGGTGATG"
-
+    # call the class ORF
     ORF_list = ORF1.finding_ORF(seq)
     print(ORF_list)
+    # call the class BLASTN
     BLAST.blasten(ORF_list)
 
