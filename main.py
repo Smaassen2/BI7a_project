a6f523c0477d86674ee9d85142f7e6745ef2fef1
Smaassen2 <71068315+Smaassen2@users.noreply.github.com>
2022-03-31 13:44:13 +0200

    versie 7

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from BLASTN import BLAST
 
 
 if __name__ == '__main__':
-    seq = "AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTG" \
-          "GATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG"
+    seq = "ATGGTGATG"
+
     ORF_list = ORF1.finding_ORF(seq)
     print(ORF_list)
     BLAST.blasten(ORF_list)
